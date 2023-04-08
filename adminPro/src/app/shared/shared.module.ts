357402a4fcfd029e5ce90eb38b7fcd9384141164
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-04-08 00:07:27 -0400

    Modificados los modulos para que sean personalizados

---
+++ adminPro/src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
