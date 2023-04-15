25a237e46664d9f1052f7ae4fa27f017d805630b
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-04-15 14:28:05 -0400

    Preparando el camino para hacer graficas personalizadas

--- adminPro/src/app/components/components.module.ts
+++ adminPro/src/app/components/components.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+/* GRAFICAS MODULO */
+import { NgChartsModule } from 'ng2-charts';
+
 /* COMPONENTES */
 import { IncrementadorComponent } from './incrementador/incrementador.component';
+import { DonaComponent } from './dona/dona.component';
 
 @NgModule({
   declarations: [
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ],
   exports: [
     IncrementadorComponent,
+    DonaComponent
   ],
   imports: [
     CommonModule,
+    NgChartsModule,
     FormsModule
   ]
 })
