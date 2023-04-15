d884dc6ecdd53202e68ea7c5b4f2e6257f062fb8
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-04-15 01:07:44 -0400

    Agregado el Output para emitir un valor

--- adminPro/src/app/components/incrementador/incrementador.component.ts
+++ adminPro/src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,15 +8,21 @@ import { Component, Input } from '@angular/core';
 })
 export class IncrementadorComponent {
 
-  // @Input('valor') progreso: number = 40;
-  @Input() progreso: number = 40;
+  @Input('valor') progreso: number = 40;
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor = (valor: number) => {
 
-    if (this.progreso >= 100 && valor >= 0) { return this.progreso = 100; };
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      return this.progreso = 100;
+    } else if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
+      return this.progreso = 0
+    };
 
-    if (this.progreso <= 0 && valor < 0) { return this.progreso = 0 };
-
+    this.valorSalida.emit(this.progreso);
     return this.progreso = this.progreso + valor;
   };
 
