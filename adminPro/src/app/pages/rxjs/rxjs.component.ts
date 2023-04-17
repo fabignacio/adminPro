33dca0f5a81c136ad2142bfc11d75187acef0d05
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-04-16 22:21:52 -0400

    Implementacion del metodo retry

--- adminPro/src/app/pages/rxjs/rxjs.component.ts
+++ adminPro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, retry } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -11,6 +11,8 @@ export class RxjsComponent {
 
   constructor() {
 
+
+
     const obs$ = new Observable(observer => {
 
       let i = -1;
@@ -20,12 +22,13 @@ export class RxjsComponent {
         i++;
         observer.next(i);
 
-        if (i === 10) {
+        if (i === 5) {
           clearInterval(intervalo);
           observer.complete();
         };
 
         if (i === 2) {
+          i = 0;
           observer.error('i llego al valor de 2');
         };
 
@@ -33,13 +36,21 @@ export class RxjsComponent {
 
     });
 
-    obs$.subscribe(
-      valor => console.log('Subs', valor),
-      err => console.warn('Error:', err),
-      () => console.info('Obs terminado') //Aqui se manda un valor cuando se completa el observable
-    );
+    obs$
+      .pipe(
+        retry(1) //Entre parentesis va la cantidad de veces que quiero que lo vuelva a intentar
+      )
+      .subscribe(
+        {
+          next: (valor) => console.log('Subs', valor),
+          error: (err) => console.warn(err),
+          complete() {
+            console.info('Obs terminado') //Aqui se manda un valor cuando se completa el observable
+          },
+        }
+      );
 
-  }
+  };
 
   ngOnInit(): void {
 
