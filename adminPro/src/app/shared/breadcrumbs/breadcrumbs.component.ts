596c9a4aba5b24e8291b3c61feddc171a1c1ca7e
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-04-17 14:04:40 -0400

    Implementado metodo ngOnDestroy para desuscribirse del observable

--- adminPro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ adminPro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,20 +11,25 @@ import { filter, map } from 'rxjs';
 export class BreadcrumbsComponent {
 
   public titulo: string = '';
+  public tituloSubs$: Subscription;
 
-  constructor(private router: Router) { this.getArgumentosRuta() };
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe({
+      next: ({ titulo }) => { this.titulo = titulo }
+    });
+  };
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  };
 
   getArgumentosRuta = () => {
 
-    this.router.events
+    return this.router.events
       .pipe(
         filter((event): event is ActivationEnd => event instanceof ActivationEnd),
         filter((event: ActivationEnd) => event.snapshot.firstChild === null),
         map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe({
-        next: ({ titulo }) => { this.titulo = titulo }
-      });
-
-  }
+      );
+  };
 };
